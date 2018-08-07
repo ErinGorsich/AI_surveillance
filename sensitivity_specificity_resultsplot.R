bd963a876aa494961ac33ea0c58922707c0270df
Unknown <hodohert@rams.colostate.edu>
2018-08-06 21:58:12 -0600

    poster figure

--- sensitivity_specificity_resultsplot.R
+++ sensitivity_specificity_resultsplot.R
@@ -7,45 +7,57 @@
 library(ggplot2)
 
 #read in fit data from selected model
-fit <- readRDS("~/Github/queens_icar_fit.rds")
+fit <- readRDS("~/Github/short_queens_fit.rds")
 
-dabbling.se.median <- quantile.mcmc.list(fit[,1])[3]
-dabbling.se.95 <- quantile.mcmc.list(fit[,1])[5]
-dabbling.se.min <- quantile.mcmc.list(fit[,1])[1]
-dabbling.sp.median <- quanitle.mcmc.list(fit[,4])[3]
-dabbling.sp.95 <- quantile.mcmc.list(fit[,4])[5]
-dabbling.sp.min <- quantile.mcmc.list(fit[,4])[1]
+dabbling.se.median <- quantile(fit[,1])[3]
+dabbling.se.95 <- quantile(fit[,1], probs = 0.95)
+dabbling.se.min <- quantile(fit[,1])[1]
 
-diving.se.median <- quantile.mcmc.list(fit[,2])[3]
-diving.se.95 <- quantile.mcmc.list(fit[,2])[5]
-diving.se.min <- quantile.mcmc.list(fit[,2])[1]
-diving.sp.median <- quantile.mcmc.list(fit[,5])[3]
-diving.sp.95 <- quantile.mcmc.list(fit[,5])[5]
-diving.sp.min <- quantile.mcmc.list(fit[,5])[1]
+dabbling.sp.median <- quantile(fit[,4])[3]
+dabbling.sp.95 <- quantile(fit[,4], probs = 0.95)
+dabbling.sp.min <- quantile(fit[,4])[1]
 
-geese.se.median <- quantile.mcmc.list(fit[,3])[3]
-geese.se.95 <- quantile.mcmc.list(fit[,3])[5]
-geese.se.min <- quantile.mcmc.list(fit[,3])[1]
-geese.sp.median <- quantile.mcmc.list(fit[,6])[3]
-geese.sp.95 <- quantile.mcmc.list(fit[,6])[5]
-geese.sp.mi <- quantile.mcmc.list(fit[,6])[1]
+diving.se.median <- quantile(fit[,2])[3]
+diving.se.95 <- quantile(fit[,2], probs = 0.95)
+diving.se.min <- quantile(fit[,2])[1]
 
-plot <- data.frame(dabbling.sensitivity = c("dabbling.se.min", "dabbling.se.median", "dabbling.se.95"),
-                   dabbling.specificity = c("dabbling.sp.min", "dabblin.sp.median", "dabbling.sp.95"),
-                   diving.sensitivity = c("diving.se.min", "diving.se.median", "diving.se.95"), 
-                   diving.specificity = c("diving.sp.min", "diving.sp.median", "diving.sp.95"),
-                   geese.sensitivity = c("geese.se.min", "geese.se.median", "geese.se.95"), 
-                   geese.specificity = c("geese.sp.min", "geese.sp.median", "geese.sp.95"))
+diving.sp.median <- quantile(fit[,5])[3]
+diving.sp.95 <- quantile(fit[,5], probs = 0.95)
+diving.sp.min <- quantile(fit[,5])[1]
+
+geese.se.median <- quantile(fit[,3])[3]
+geese.se.95 <- quantile(fit[,3], probs = 0.95)
+geese.se.min <- quantile(fit[,3])[1]
+
+geese.sp.median <- quantile(fit[,6])[3]
+geese.sp.95 <- quantile(fit[,6], probs = 0.95)
+geese.sp.min <- quantile(fit[,6])[1]
+
 plot.se <- data.frame(species = c("1", "2", "3"), 
-                      minimum = c("dabbling.se.min", "diving.se.min", "geese.se.min"),
-                      median = c("dabbling.se.median", "diving.se.median", "geese.se.median"),
-                      ninetyfifth = c("dabbling.se.95", "diving.se.95", "geese.se.95"))
+                      minimum = c(dabbling.se.min, diving.se.min, geese.se.min),
+                      median = c(dabbling.se.median, diving.se.median, geese.se.median),
+                      ninetyfifth = c(dabbling.se.95, diving.se.95, geese.se.95))
 plot.sp <- data.frame(species = c("1", "2", "3"),
-                      minimum = c("dabbling.sp.min", "diving.sp.min", "geese.sp.min"),
-                      median = c("dabbling.sp.median", "dabbling.sp.median", "geese.sp.median"),
-                      ninetyfifth = c("dabbling.sp.95", "dabbling.sp.95", "geese.sp.95"))
+                      minimum = c(dabbling.sp.min, diving.sp.min, geese.sp.min),
+                      median = c(dabbling.sp.median, dabbling.sp.median, geese.sp.median),
+                      ninetyfifth = c(dabbling.sp.95, dabbling.sp.95, geese.sp.95))
 
+jpeg("~/Honors Thesis/Thesis Work/Figures/sensitivityplot.jpeg")
 ggplot(data = plot.se) +
-  geom_points(aes(x = median, y = species)) +
-  error_bar(aes(xmin = minimum, xmax = ninetyfifth))
+  geom_point(aes(x = median, y = species), color = "darkgreen") +
+  geom_errorbarh(aes(x = median, y = species, xmin = minimum, xmax = ninetyfifth), height = .1, color = "darkgreen") +
+  ylab("Species Group") +
+  xlab("Estimated Diagnostic Test Sensitivity") +
+  scale_x_continuous(breaks = breaks, limits = c(0, 0.7)) +
+  theme(panel.background=element_blank(), axis.ticks = element_line(), axis.line = element_line())
+dev.off()
 
+jpeg("~/Honors Thesis/Thesis Work/Figures/specificityplot.jpeg")
+ggplot(data = plot.sp) +
+  geom_point(aes(x = median, y = species), color = "darkgreen") +
+  geom_errorbarh(aes(x = median, y = species, xmin = minimum, xmax = ninetyfifth), height = .1, color = 'darkgreen') +
+  ylab("Species Group") +
+  xlab("Estimated Diagnostic Test Specificity") +
+  scale_x_continuous(breaks = breaks, limits = c(0.9, 1)) +
+  theme(panel.background=element_blank(), axis.ticks = element_line(), axis.line = element_line())
+dev.off()
