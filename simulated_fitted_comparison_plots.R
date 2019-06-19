830a4f9a7f79d9fdc4d9af5f7baad791ed8611c5
Unknown <hodohert@rams.colostate.edu>
2019-06-19 17:26:13 -0600

    finished

--- simulated_fitted_comparison_plots.R
+++ simulated_fitted_comparison_plots.R
@@ -4,12 +4,14 @@
 #########################################################################################
 #########################################################################################
 
-base.fit <- readRDS("/home/webblab/Documents/HP/base_model_fit_sim_6.rds")
-lambda <- readRDS('/home/webblab/Documents/HP/sim_data_6_lamda.rds')
+# base.fit <- readRDS("/home/webblab/Documents/HP/base_model_fit_sim_6.rds")
+# lambda <- readRDS('/home/webblab/Documents/HP/sim_data_6_lamda.rds')
+base.fit <- readRDS('~/Github/base_model_fit_sim_6.rds')
+lambda <- readRDS('~/Github/sim_data_6_lamda.rds')
 
 nmonths <- 12
 nyears <- 5
-nhuc <- 191
+nhucs <- 191
 nparam <- 2+(12*5*191)
 nchains <- 3
 
@@ -26,7 +28,7 @@ for (l in 1:nparam){
     z[1,l] <- quantile(y[,l], probs=0.5)
   }
 }
-mean <- array(dim=c(nmonths, nyears, nhuc), NA)
+mean <- array(dim=c(nmonths, nyears, nhucs), NA)
 for (i in 1:nmonths) {
   for (j in 1:nyears) {
     for (k in 1:nhucs) {
@@ -35,52 +37,96 @@ for (i in 1:nmonths) {
     }
   }
 }
-mean.huc <- matrix(nrow=nhuc, ncol=1, NA)
-for (k in 1:nhuc){
+#calculate a huc level mean of the lambda estimate
+mean.huc <- matrix(nrow=nhucs, ncol=1, NA)
+stand.dev.huc <- matrix(nrow=nhucs, ncol=1, NA)
+for (k in 1:nhucs){
   mean.huc[k,1] <- quantile(mean[,,k], probs=0.5)
+  stand.dev.huc[k,1] <- sd(mean[,,k])
 }
 mean.all <- quantile(mean.huc[,1], probs=0.5)
-stand.dev <- sd(mean.huc[,1])
-outlier <- matrix(nrow=nhuc, ncol=1, NA)
-for (k in 1:nhuc){
-  if (mean.huc[k,1]>mean.all+stand.dev) {
+#determine if a huc is an outlier
+#outlier = outside +/- overall standard deviation
+stand.dev.all <- sd(mean.huc[,1])
+outlier <- matrix(nrow=nhucs, ncol=1, NA)
+for (k in 1:nhucs){
+  if (mean.huc[k,1]>mean.all+stand.dev.all) {
     outlier[k,1] <- TRUE
-  } else if (mean.huc[k,1]<mean.all-stand.dev) {
+  } else if (mean.huc[k,1]<mean.all-stand.dev.all) {
     outlier[k,1] <- TRUE
   } else {
     outlier[k,1] <- FALSE
   }
 }
 outlier <- as.data.frame(outlier)
-
-difference <- array(dim=c(nmonths, nyears, nhuc), NA)
-for (i in 1:nmonths){
-  for (j in 1:nyears){
-    for (k in 1:nhucs){
-      difference[i,j,k] <- lambda[i,j,k]-mean[i,j,k]
-    }
-  }
+#calculate a mean "true" lambda for each huc
+mean.huc.true <- matrix(nrow=nhucs, ncol=1, NA)
+sd.huc.true <- matrix(nrow=nhucs, ncol=1, NA)
+max.huc.true <- matrix(nrow=nhucs, ncol=1, NA)
+for (k in 1:nhucs){
+  mean.huc.true[k,1] <- quantile(lambda[,,k], probs=0.5)
+  sd.huc.true[k,1] <- sd(lambda[,,k])
+  max.huc.true[k,1] <- max(lambda[,,k])
+}
+#find the difference between the "true" lambda mean and 
+# estimated lambda mean for each huc
+# difference <- array(dim=c(nmonths, nyears, nhucs), NA)
+# for (i in 1:nmonths){
+#   for (j in 1:nyears){
+#     for (k in 1:nhucs){
+#       difference[i,j,k] <- lambda[i,j,k]-mean[i,j,k]
+#     }
+#   }
+# }
+mean.difference <- matrix(ncol=1, nrow=nhucs, NA)
+for (k in 1:nhucs){
+  mean.difference[k,1] <- mean.huc[k,1] - mean.huc.true[k,1]
 }
 
-plot <- data.frame(month = rep(seq(1,12), times = nyears*nhuc),
-         year = rep(seq(1,5), each = nmonths, times=nhuc),
-         huc = rep(seq(1,191), each = nmonths*nyears),
-         lambda.true = NA, lambbda.est = NA, difference = NA, mean = NA, outlier = NA)
-for (i in 1:nmonths) {
-  for (j in 1:nyears) {
-    for (k in 1:nhuc) {
-      plot$lambda.true[plot$month==i & plot$year==j & plot$huc==k] <- lambda[i,j,k]
-      plot$lambbda.est[plot$month==i & plot$year==j & plot$huc==k] <- mean[i,j,k]
-      plot$difference[plot$month==i & plot$year==j & plot$huc==k] <- difference[i,j,k]
-      plot$mean[plot$huc==k] <- mean.huc[k,1]
-      plot$outlier[plot$huc==k] <- outlier$V1[k]
-    }
-  }
-}
+plot <- data.frame(huc = seq(1, 191), lambda.true = mean.huc.true, 
+                   lambda.true.max = max.huc.true, lambda.est = mean.huc, 
+                   sd.true = sd.huc.true, sd.est = stand.dev.huc, 
+                   difference = mean.difference, outlier = outlier$V1)
+
 ##################################################################################################
 #make plots
 ##################################################################################################
 
 plot.1 <- plot[plot$outlier==TRUE, ]
+for (i in 1:length(plot.1$lambda.est)){
+  plot.1$ci.95.min[i] <- 2*plot.1$sd.est[i]+plot.1$lambda.est[i]
+  plot.1$ci.95.max[i] <- 2*plot.1$sd.est[i]-plot.1$lambda.est[i]
+}
 
-outlier.hucs <- ggplot(plot, aes(x=huc, y=da))
+outlier.hucs <- ggplot(plot.1, aes(x=huc)) +
+  geom_point(aes(y = lambda.est), color = "darkblue") +
+  geom_errorbar(aes(ymin=ci.95.min, ymax=ci.95.max), color="darkblue") +
+  geom_point(aes(y= lambda.true), color = "darkred") +
+  geom_errorbar(aes(ymin=0, ymax=lambda.true.max), color="darkred")+
+  geom_hline(aes(yintercept=mean.all)) +
+  theme_minimal() +
+  ggtitle("Comparison of Simulated and Estimated True Prevalence for Outlier Watersheds")+
+  xlab("Watershed") +
+  ylab("Prevalence")
+
+plot %>% arrange(difference)
+
+plot.2 <- plot[1:20, ]  
+
+smallest.diff <- ggplot(plot.2, aes(x=huc)) +
+  geom_point(aes(y = difference), color="darkgreen") +
+  geom_errorbar(aes(ymin=lambda.true, ymax=lambda.est), color="darkgreen")+
+  theme_minimal()+
+  ggtitle("20 Watersheds with the Smallest Difference between True and Estimated Prevalence")+
+  xlab("Watershed") +
+  ylab("Difference")
+
+plot.3 <- plot[(191-20):191, ]
+
+largest.diff <- ggplot(plot.3, aes(x=huc)) +
+  geom_point(aes(y=difference, color="darkorange"))+
+  geom_errorbar(aes(ymin=lambda.true, ymax=lambda.est), color="darkorange")+
+  theme_minimal()+
+  ggtitle("20 Watersheds with the Largest Difference between Tre and Estimated Prevalence")+
+  xlab("Watershed")+
+  ylab("Difference")
